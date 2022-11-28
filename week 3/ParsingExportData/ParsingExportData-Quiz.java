8e14c7b813601f1bd48be1b9a18bd00f00d9528d
Jerome Pan <113123500+Ryouji-P@users.noreply.github.com>
2022-11-28 22:13:48 +0000

    Add files via upload

---
+++ week 3/ParsingExportData/ParsingExportData-Quiz.java
@@ -0,0 +1,75 @@
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class ParsingExportData 
+{
+    //This method returns a string of information about the country or returns “NOT FOUND” if there is no information about the country. 
+    public String countryInfo (CSVParser parser, String country)
+    {
+        String info = "NOT FOUND";
+        for (CSVRecord record : parser) {
+            String getCountry = record.get("Country");
+            if (getCountry.contains(country)) {
+                String getExport = record.get("Exports");
+                String getValue = record.get("Value (dollars)");
+                info = getCountry + ":" + getExport + ":" + getValue;
+            }
+        }
+        return info;
+    }
+    
+    public void listExportersTwoProducts (CSVParser parser, String exportItem1, String exportItem2)
+    {
+        for (CSVRecord record : parser) {
+            String getExport = record.get("Exports");
+            if (getExport.contains(exportItem1) && getExport.contains(exportItem2)) {
+                String getCountry = record.get("Country");
+                System.out.println(getCountry);
+            }
+        }
+        
+    }
+    
+    public int numberOfExporters (CSVParser parser, String exportItem) 
+    {
+        int count = 0;
+        for (CSVRecord record : parser) {
+            String getExport = record.get("Exports");
+            if (getExport.contains(exportItem)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void bigExporters (CSVParser parser, String amount) 
+    {
+        String countryValue = "";
+        for (CSVRecord record : parser) {
+            String getValue = record.get("Value (dollars)");
+            if (getValue.length() > amount.length()) {
+                String getCountry = record.get("Country");
+                countryValue = getCountry + " " + getValue;
+                System.out.println(countryValue);
+            }
+        }
+    }
+
+    //test
+    public void tester() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        String country = countryInfo(parser, "Nauru");
+        System.out.println(country);
+        
+        parser = fr.getCSVParser();
+        listExportersTwoProducts(parser, "fish", "nuts");
+        
+        parser = fr.getCSVParser();
+        int count = numberOfExporters(parser, "sugar");
+        System.out.println(count);
+        
+        parser = fr.getCSVParser();
+        bigExporters(parser, "$999,999,999,999");
+    }
+}
